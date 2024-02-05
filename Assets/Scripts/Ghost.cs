b4e98a08426150456922a79deadf8566fa654b22
JMUCF <ju398578@ucf.edu>
2024-02-05 13:59:52 -0500

    enemies move randomly on nodes

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -5,4 +5,33 @@ using UnityEngine;
 public class Ghost : MonoBehaviour
 {
     public int points = 100;
+	public Ghost ghost;
+	public Movement movement;
+	
+	private void Awake()
+	{
+		this.movement = GetComponent<Movement>();
+	}
+	
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Node node = collision.GetComponent<Node>();
+		
+		if(node != null)
+		{
+			int index = Random.Range(0, node.availableDirections.Count);
+			
+			if(node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1)
+			{
+				index++;
+				
+				if(index >= node.availableDirections.Count)
+				{
+					index = 0;
+				}
+			}
+			
+			this.ghost.movement.SetDirection(node.availableDirections[index]);
+		}
+	}
 }
